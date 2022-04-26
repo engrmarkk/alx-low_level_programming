20ab40b09090c0f0e1deec0bc131f6704bd12f3e
engrmarkk <100721103+engrmarkk@users.noreply.github.com>
2022-04-26 13:26:47 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,85 +1,54 @@
 #include "main.h"
-
-/**
- * carry_from_prev - carry overflow from previous index to current index
- * @b: the buffer to operate on
- * @i: the index to carry to
- *
- * Return: void
- */
-void carry_from_prev(char *b, int i)
-{
-	b[i] = b[i - 1] / 10;
-	b[i - 1] %= 10;
-}
-
-
-/**
- * find_str_end - find the end of a string
- * @s: the string to search
- *
- * Return: a pointer to the end of the string
- */
-char *find_str_end(char *s)
-{
-	char *s_end;
-
-	for (s_end = s; *s_end; ++s_end)
-		;
-	return (s_end);
-}
-
-
+#include <stdio.h>
 /**
  * infinite_add - adds two numbers
- * @n1: the first number to add
- * @n2: the other number to add
- * @r: the buffer to store the result
- * @size_r: the size of the buffer
- *
- * Return: a pointer to the buffer r, unless the result does not fit into r,
- * in which case return 0.
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
+ * Return: the pointer to dest.
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	char *n1_end, *n2_end;
-	int i, j;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	for ( ; n1[1] && *n1 == '0'; ++n1)
-		;
-	for ( ; n2[1] && *n2 == '0'; ++n2)
-		;
-	n1_end = find_str_end(n1);
-	n2_end = find_str_end(n2);
-	for (i = 0; n1 < n1_end || n2 < n2_end; ++i)
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
+	else
+		bg = c2;
+	if (size_r <= bg + 1)
+		return (0);
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
 	{
-		if (i + 1 >= size_r)
-			return (0);
-		if (i && r[i - 1] > 9)
-			carry_from_prev(r, i);
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
 		else
-			r[i] = 0;
-		if (n1 < n1_end)
-			r[i] += *(--n1_end) - '0';
-		if (n2 < n2_end)
-			r[i] += *(--n2_end) - '0';
+			add = 0;
+		if (op > 0)
+		*(r + bg) = (op % 10) + 48;
+		else
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
+		else
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
+		else
+			dr2 = 0;
+		bg--, size_r--;
 	}
-	if (r[i - 1] > 9)
-	{
-		if (i + 1 >= size_r)
-			return (0);
-		carry_from_prev(r, i++);
-	}
-	r[i--] = '\0';
-	for (j = 0; j < i; --i, ++j)
-	{
-		r[i] ^= r[j];
-		r[j] ^= r[i];
-		r[i] ^= r[j];
-		r[i] += '0';
-		r[j] += '0';
-	}
-	if (j == i)
-		r[i] += '0';
-	return (r);
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
